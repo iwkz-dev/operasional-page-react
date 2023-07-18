2c4747b864314729e1e64eec860533bf1f031c41
fathonitr <ododmengapa@gmail.com>
2023-07-18 10:07:14 +0200

    working paypal sandbox

--- src/components/PayPalButton/index.jsx
+++ src/components/PayPalButton/index.jsx
@@ -1,10 +1,13 @@
 import { Container, Box, Paper, Button } from "@mui/material"
-const DonateBtn=()=>{
- return   <Box sx={{ p: 4, textAlign:"center"}}>
-                        <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top">
-                            <Button variant="contained" size="large" href={"https://www.sandbox.paypal.com/cgi-bin/webscr"}>Sedekah Sekarang</Button>
-                        </form>
-                    </Box>
+const DonateBtn = () => {
+    return <Box sx={{ p: 4, textAlign: "center" }}>
+        <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top">
+            <input type="hidden" name="cmd" value="_s-xclick" />
+            <input type="hidden" name="hosted_button_id" value="QD7NJS5R6VSWE" />
+            <input type="hidden" name="item_name" value="infaq_operasional_masjid_alfalah" />
+            <Button variant="contained" size="large" type="submit">Sedekah Sekarang</Button>
+        </form>
+    </Box>
 }
 
 export default DonateBtn
